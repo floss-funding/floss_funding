6a7ae6ca1ec62040656ee5b9de31758be83d1f6a
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 05:14:24 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/floss_funding/version.rb
+++ lib/floss_funding/version.rb
@@ -6,4 +6,6 @@ module FlossFunding
     # Current gem version.
     VERSION = "1.0.0.pre.beta.1"
   end
+  # Current gem version exposed at the traditional constant location.
+  VERSION = Version::VERSION # Traditional Constant Location
 end
